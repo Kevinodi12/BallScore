d87649edf057c2ccbfb470c137ebc03e159def14
Kevinodi12 <kevin_odi@hotmail.com>
2023-10-30 13:56:34 -0300

    cambio diseno tarjeta y agrego funcionalidades

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -43,7 +43,7 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
 
 $$(document).on('page:init', '.page[data-name="login"]', function (e) {
   $$("#btnInicioSesion").on("click", fnIniciarSesion);
- 
+
 })
 
 
@@ -58,14 +58,24 @@ $$(document).on('page:init', '.page[data-name="about"]', function (e) {
 })
 
 $$(document).on('page:init', '.page[data-name="inicio"]', function (e) {
-llamarPartidosChampions();
-llamarPartidosCopadelaliga();
+$$("#boton-plegable").on("click", fnPlegartarjetas);
+//llamarPartidosChampions();
+//llamarPartidosCopadelaliga();
+//llamarPartidosPremierLeague();
 //pruebaApi();
 });
 
 /* Mis funciones */
 const apiKey = '71755f9287199e45805472d2ecbdaa14';
 var email, clave, nombre, apellido;
+
+function fnPlegartarjetas() {
+  var tarjeta = $$(this).closest('.plegable-card');
+ 
+  tarjeta.find('.plegable-content').slideToggle();
+}
+
+
 
 function fnIniciarSesion() {
   email = $$("#loginEmail").val();
@@ -99,173 +109,249 @@ function fnIniciarSesion() {
 }
 
 
-
-//Llama partidos del fixture de la champions del dia y datos de la liga
-
-function llamarPartidosChampions() {
-    var myHeaders = new Headers();
-    myHeaders.append("x-rapidapi-key", apiKey);
-    myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
-  
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-  
-    var leagueInfoContainer = document.getElementById("leagueInfo");
-    var matchInfoContainer = document.getElementById("matchInfo");
-  
-    fetch("https://v3.football.api-sports.io/fixtures?date=2023-10-25&league=2&season=2023&timezone=America/Argentina/Buenos_Aires", requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        var partidos = data.response;
-        var liga = data.response[0].league;
-    
-          // ORIGEN DEL ERROR!
-  
-       /* leagueInfoContainer.innerHTML = `
-          <div id="liga-nombre">
-            <div id="liga-logo">
-              <img id="image-logo" src="${liga.logo}" />
-            </div>
-            <h3 class="nombre-liga">${liga.name}</h3>
-          </div>
-        `;*/
-
-
-          partidos.forEach(matchData => {
-          
-              const fechaHora = new Date(matchData.fixture.date);
-
-              const horas = fechaHora.getHours();
-              const minutos = fechaHora.getMinutes();
-
-            
-              const horaYMinutos = `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}`;
-
-              let html = `
-
-    
-              <div class="equipo-info">
-              <div id="logo-equipo1">
-                  <img src="${matchData.teams.home.logo}" id="equipo1-logo" class="logo-equipo1" />
-              </div>
-              <div id="nombre-equipo1" class="nombre-equipo">${matchData.teams.home.name}</div>
-              <div id="marcador-equipo1" class="marcador-equipo1">
-                  <p id="goles-local">${matchData.goals.home}</p>
-              </div>
-          </div>
-          <div class="horario-notificacion">
-              <div id="horario-partido"><p id="status-partido">${horaYMinutos}</p></div>
-              <div id="notificacion-desactiva-fixture"><i class="f7-icons">bell_slash</i></div>
-          </div>
-          </div>
-          <div class="equipo-info2">
-              <div id="logo-equipo2">
-                  <img src="${matchData.teams.away.logo}" id="equipo2-logo" class="logo-equipo2" />
-              </div>
-              <div id="nombre-equipo2" class="nombre-equipo2">${matchData.teams.away.name}</div>
-              <div id="marcador-equipo2" class="marcador-equipo2">
-                  <p id="goles-visitante">${matchData.goals.away}</p>
-              </div>
-                  
-              `;
-
-              let card = document.createElement('div');
-              card.className = 'card';
-              card.innerHTML = html;
-              matchInfoContainer.appendChild(card);
-          });
-      })
-      .catch(error => console.log('error', error));
-}
-
-
-
-
 //LLama partidos del fixture de la copa de la liga y datos de la liga
 
 
 function llamarPartidosCopadelaliga() {
-    var myHeaders = new Headers();
-    myHeaders.append("x-rapidapi-key", apiKey);
-    myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+  var myHeaders = new Headers();
+  myHeaders.append("x-rapidapi-key", apiKey);
+  myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
   
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-  
-    const leagueInfoContainer = document.getElementById("leagueInfo2");
-    const matchInfoContainer = document.getElementById("matchInfo2");
-  
-    fetch("https://v3.football.api-sports.io/fixtures?date=2023-10-25&league=1032&season=2023&timezone=America/Argentina/Buenos_Aires", requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        var partidos = data.response;
-        var liga = data.response[0].league;
-      
-  
-       /* leagueInfoContainer.innerHTML = `
-          <div id="partidos-fecha"></div>
-          <div id="liga-nombre">
-            <div id="liga-logo">
-              <img id="image-logo" src="${liga.logo}" />
+  var matchInfoContainer = document.getElementById("matchInfo");
+
+  fetch("https://v3.football.api-sports.io/fixtures?date=2023-10-30&league=1032&season=2023&timezone=America/Argentina/Buenos_Aires", requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      var partidos = data.response;
+      var liga = data.response[0].league;
+
+ 
+      let html = `
+        <div class="card">
+          <div class="card-header">
+            <div id="liga-nombre">
+              <div id="liga-logo">
+                <img id="image-logo" src="${liga.logo}" />
+              </div>
+              <h3 class="nombre-liga">${liga.name}</h3>
             </div>
-            <h3 class="nombre-liga">${liga.name}</h3>
           </div>
-        `;
-          */
+          <hr>
+          <div class="card-content card-content-padding">
+      `;
 
-          partidos.forEach(matchData => {
-          
-              const fechaHora = new Date(matchData.fixture.date);
+      partidos.forEach(matchData => {
+        const fechaHora = new Date(matchData.fixture.date);
+        const horas = fechaHora.getHours();
+        const minutos = fechaHora.getMinutes();
+        const horaYMinutos = `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}`;
 
-            
-              const horas = fechaHora.getHours();
-              const minutos = fechaHora.getMinutes();
-
-            
-              const horaYMinutos = `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}`;
-
-              var html = `
-
-    
-              <div class="equipo-info">
-              <div id="logo-equipo1">
-                  <img src="${matchData.teams.home.logo}" id="equipo1-logo" class="logo-equipo1" />
-              </div>
-              <div id="nombre-equipo1" class="nombre-equipo">${matchData.teams.home.name}</div>
-              <div id="marcador-equipo1" class="marcador-equipo1">
-                  <p id="goles-local">${matchData.goals.home}</p>
-              </div>
+        html += `
+          <div class="equipo-info">
+            <div id="logo-equipo1">
+              <img src="${matchData.teams.home.logo}" id="equipo1-logo" class="logo-equipo1" />
+            </div>
+            <div id="nombre-equipo1" class="nombre-equipo">${matchData.teams.home.name}</div>
+            <div id="marcador-equipo1" class="marcador-equipo1">
+              <p id="goles-local">${matchData.goals.home}</p>
+            </div>
           </div>
           <div class="horario-notificacion">
-              <div id="horario-partido"><p id="status-partido">${horaYMinutos}</p></div>
-              <div id="notificacion-desactiva-fixture"><i class="f7-icons">bell_slash</i></div>
-          </div>
+            <div id="horario-partido"><p id="status-partido">${horaYMinutos}</p></div>
+            <div id="notificacion-desactiva-fixture"><i class="f7-icons">bell_slash</i></div>
           </div>
           <div class="equipo-info2">
-              <div id="logo-equipo2">
-                  <img src="${matchData.teams.away.logo}" id="equipo2-logo" class="logo-equipo2" />
-              </div>
-              <div id="nombre-equipo2" class="nombre-equipo2">${matchData.teams.away.name}</div>
-              <div id="marcador-equipo2" class="marcador-equipo2">
-                  <p id="goles-visitante">${matchData.goals.away}</p>
-              </div>
-                  
-              `;
+            <div id="logo-equipo2">
+              <img src="${matchData.teams.away.logo}" id="equipo2-logo" class="logo-equipo2" />
+            </div>
+            <div id="nombre-equipo2" class="nombre-equipo2">${matchData.teams.away.name}</div>
+            <div id="marcador-equipo2" class="marcador-equipo2">
+              <p id="goles-visitante">${matchData.goals.away}</p>
+            </div>
+          </div>
+          <hr>
+        `;
+      });
 
-              var card = document.createElement('div');
-              card.className = 'card';
-              card.innerHTML = html;
-              matchInfoContainer.appendChild(card);
-          });
-      })
-      .catch(error => console.log('error', error));
+      html += `</div></div>`; 
+      let card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = html;
+      matchInfoContainer.appendChild(card);
+    })
+    .catch(error => console.log('error', error));
 }
+
+
+function llamarPartidosPremierLeague(){
+  var myHeaders = new Headers();
+  myHeaders.append("x-rapidapi-key", apiKey);
+  myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  
+  var matchInfoContainer = document.getElementById("matchInfo2");
+
+  fetch("https://v3.football.api-sports.io/fixtures?date=2023-10-30&league=39&season=2023&timezone=America/Argentina/Buenos_Aires", requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      var partidos = data.response;
+      var liga = data.response[0].league;
+
+ 
+      let html = `
+        <div class="card">
+          <div class="card-header">
+            <div id="liga-nombre">
+              <div id="liga-logo">
+                <img id="image-logo" src="${liga.logo}" />
+              </div>
+              <h3 class="nombre-liga">${liga.name}</h3>
+            </div>
+          </div>
+          <hr>
+          <div class="card-content card-content-padding">
+      `;
+
+      partidos.forEach(matchData => {
+        const fechaHora = new Date(matchData.fixture.date);
+        const horas = fechaHora.getHours();
+        const minutos = fechaHora.getMinutes();
+        const horaYMinutos = `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}`;
+
+        html += `
+          <div class="equipo-info">
+            <div id="logo-equipo1">
+              <img src="${matchData.teams.home.logo}" id="equipo1-logo" class="logo-equipo1" />
+            </div>
+            <div id="nombre-equipo1" class="nombre-equipo">${matchData.teams.home.name}</div>
+            <div id="marcador-equipo1" class="marcador-equipo1">
+              <p id="goles-local">${matchData.goals.home}</p>
+            </div>
+          </div>
+          <div class="horario-notificacion">
+            <div id="horario-partido"><p id="status-partido">${horaYMinutos}</p></div>
+            <div id="notificacion-desactiva-fixture"><i class="f7-icons">bell_slash</i></div>
+          </div>
+          <div class="equipo-info2">
+            <div id="logo-equipo2">
+              <img src="${matchData.teams.away.logo}" id="equipo2-logo" class="logo-equipo2" />
+            </div>
+            <div id="nombre-equipo2" class="nombre-equipo2">${matchData.teams.away.name}</div>
+            <div id="marcador-equipo2" class="marcador-equipo2">
+              <p id="goles-visitante">${matchData.goals.away}</p>
+            </div>
+          </div>
+          <hr>
+        `;
+      });
+
+      html += `</div></div>`; 
+      let card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = html;
+      matchInfoContainer.appendChild(card);
+    })
+    .catch(error => console.log('error', error));
+}
+
+
+
+
+
+//Llama partidos del fixture de la champions del dia y datos de la liga
+
+function llamarPartidosChampions() {
+  var myHeaders = new Headers();
+  myHeaders.append("x-rapidapi-key", apiKey);
+  myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+ 
+  var matchInfoContainer = document.getElementById("matchInfo3");
+
+  fetch("https://v3.football.api-sports.io/fixtures?date=2023-10-30&league=2&season=2023&timezone=America/Argentina/Buenos_Aires", requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      var partidos = data.response;
+      var liga = data.response[0].league;
+
+    
+      let html = `
+        <div class="card">
+          <div class="card-header">
+            <div id="liga-nombre">
+              <div id="liga-logo">
+                <img id="image-logo" src="${liga.logo}" />
+              </div>
+              <h3 class="nombre-liga">${liga.name}</h3>
+            </div>
+          </div>
+          <hr>
+          <div class="card-content card-content-padding">
+      `;
+
+      partidos.forEach(matchData => {
+        const fechaHora = new Date(matchData.fixture.date);
+        const horas = fechaHora.getHours();
+        const minutos = fechaHora.getMinutes();
+        const horaYMinutos = `${horas.toString().padStart(2, '0')}:${minutos.toString().padStart(2, '0')}`;
+
+        html += `
+          <div class="equipo-info">
+            <div id="logo-equipo1">
+              <img src="${matchData.teams.home.logo}" id="equipo1-logo" class="logo-equipo1" />
+            </div>
+            <div id="nombre-equipo1" class="nombre-equipo">${matchData.teams.home.name}</div>
+            <div id="marcador-equipo1" class="marcador-equipo1">
+              <p id="goles-local">${matchData.goals.home}</p>
+            </div>
+          </div>
+          <div class="horario-notificacion">
+            <div id="horario-partido"><p id="status-partido">${horaYMinutos}</p></div>
+            <div id="notificacion-desactiva-fixture"><i class="f7-icons">bell_slash</i></div>
+          </div>
+          <div class="equipo-info2">
+            <div id="logo-equipo2">
+              <img src="${matchData.teams.away.logo}" id="equipo2-logo" class="logo-equipo2" />
+            </div>
+            <div id="nombre-equipo2" class="nombre-equipo2">${matchData.teams.away.name}</div>
+            <div id="marcador-equipo2" class="marcador-equipo2">
+              <p id="goles-visitante">${matchData.goals.away}</p>
+            </div>
+          </div>
+          <hr>
+        `;
+      });
+
+      html += `</div></div>`; 
+      let card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = html;
+      matchInfoContainer.appendChild(card);
+    })
+    .catch(error => console.log('error', error));
+}
+
+
+
 
 
 
